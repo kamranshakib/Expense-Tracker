60df0ad82210bbf36d1d4ece7e8f609089a57eec
Kamran Shakib <kamranshakib371@gmail.com>
2026-01-06 21:18:35 -0800

    Successfully conntected to database

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,17 +2,19 @@ import express  from "express";
 import cors from "cors";
 import dotenv from "dotenv"
 import path from "path"
+import MongoDB from "./config/Db.config.js"
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-// Middleware 
+// Middleware  
 app.use(cors({
     origin: process.env.CLIENT_URL || "*",
     methods: ["GET","POST","DELETE","PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }))
 app.use(express.json())
+MongoDB();
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
