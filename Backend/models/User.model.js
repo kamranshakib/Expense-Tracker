59b456ac8db68cc27aabee7328ed45b30f991904
Kamran Shakib <kamranshakib371@gmail.com>
2026-01-06 21:58:54 -0800

    create a Schema for User

---
+++ Backend/models/User.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password:{
+    type: String,
+    required: true
+  },
+  profileImageUrl :{
+    type: String,
+    default: null
+  } 
+},{timestamps: true});
+
+// hash password
+UserSchema.pre("save", async (next)=>{
+    if(!this.modified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)  
+next()  
+})
+
+// compare password
+UserSchema.method.comparePassword = async (condidatePassword)=>{
+   return await bcrypt.compare(condidatePassword,this.password)
+}
+
+
+export default UserSchema;
