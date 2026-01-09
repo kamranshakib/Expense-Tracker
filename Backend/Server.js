aa5c09ea08a3bb5785919487c9fe10669b0807de
Kamran Shakib <kamranshakib371@gmail.com>
2026-01-09 08:47:51 -0800

    Add multer upload middleware and serve uploaded files statically

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
 import MongoDB from "./config/Db.config.js";
 import authRoutes from "./routes/Auth.routes.js"
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
@@ -20,5 +25,8 @@ app.use(
 );
 app.use(express.json());
 app.use("/api/v1/auth",authRoutes)
+// uploads
+app.use("/uploads",express.static(path.join(__dirname, "uploads")))
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+ 
--- Backend/routes/Auth.routes.js
+++ Backend/routes/Auth.routes.js
@@ -2,10 +2,23 @@ import experss from "express"
 const Router = experss.Router();
 import * as authController from "../controller/auth.controller.js"
 import { protect } from "../middleware/Auth.middleware.js";
+import upload from "../middleware/Upload.middleware.js";
 
 Router.get("/login",authController.loginUser)
 Router.post("/register",authController.registerUser)
 Router.get("/getUser",protect,authController.userInfo)
+
+Router.post("/upload-image",upload.single("image"),(req,res)=>{
+    if(!req.file){
+        return res.status(400).json({
+            message: "No file uploaded "
+        })
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads${req.file.filename}`
+    res.status(200).json({
+        imageUrl
+    })
+})
 
  
 export default Router
