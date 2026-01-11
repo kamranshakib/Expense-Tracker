1615bda3f8b6bdd29986a284546fd0dfa894833c
Kamran Shakib <kamranshakib371@gmail.com>
2026-01-10 23:53:18 -0800

    write download code for income xlsx

--- Backend/controller/income.controller.js
+++ Backend/controller/income.controller.js
@@ -1,4 +1,5 @@
 import express from "express";
+import xlsx from "xlsx";
 import Income from "../models/income.model.js";
 
 // add income
@@ -7,16 +8,16 @@ export const addIncome = async (req, res) => {
   console.log(req.user);
 
   try {
-    const {amount, source, icon, date} = req.body;
-    if ((!amount || !source || !date)) {
-      return res.status(400).json({ message: "All fields are required"});
+    const { amount, source, icon, date } = req.body;
+    if (!amount || !source || !date) {
+      return res.status(400).json({ message: "All fields are required" });
     }
     const newIncome = new Income({
       userId,
       amount,
       source,
       icon,
-      date: new Date(date)
+      date: new Date(date),
     });
     await newIncome.save();
     res.status(200).json({
@@ -31,33 +32,48 @@ export const addIncome = async (req, res) => {
 
 // get all  income
 export const getAllIncome = async (req, res) => {
-    const userId = req.user._id;
-    try {
-        const getIncome = await Income.find({userId}).sort({date: -1}); 
-        res.status(200).json(getIncome)
-
-    } catch (error) {
-        res.status(500).json({
-            message: "Server Error"
-        })
-    }
+  const userId = req.user._id;
+  try {
+    const getIncome = await Income.find({ userId }).sort({ date: -1 });
+    res.status(200).json(getIncome);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
 };
 
 // delete  income
 export const deleteIncome = async (req, res) => {
-  const userId = req.user._id; 
-
   try {
     const deleteIncome = await Income.findByIdAndDelete(req.params.id);
-    return res.status(200).json({message: "Income deleted Successfully"});
+    return res.status(200).json({ message: "Income deleted Successfully" });
   } catch (error) {
-      res.status(500).json({
-        message: "Server Error "
-      })
+    res.status(500).json({
+      message: "Server Error ",
+    });
   }
 };
 
-
 // download income
-export const downloadIncomeExcel = (req, res) => {};
+export const downloadIncomeExcel = async (req, res) => {
+  const userId = req.user._id;
+  try {
+    const income = await Income.find({ userId }).sort({ date: -1 });
 
+    const date = income.map((item) => ({
+      Source: item.source,
+      Amount: item.amount,
+      Date: item.date,
+    }));
+    const workSheet = xlsx.utils.json_to_sheet(date);
+    const workBook = xlsx.utils.book_new();
+    xlsx.utils.book_append_sheet(workBook, workSheet, "Icnome");
+    xlsx.writeFile(workBook, "icnome_details.xlsx");
+    res.download("icnome_details.xlsx");
+  } catch (error) {
+    res.status(500).josn({
+      message: "Server Error"
+    })
+  }
+};
