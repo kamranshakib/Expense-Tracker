85291ab7fa27c6b7ba679cec2f2eda982869416b
Kamran Shakib <kamranshakib371@gmail.com>
2026-02-12 20:50:55 +0430

    fix: resolve dashboard route 404 error

--- Backend/routes/Dashboard.routes.js
+++ Backend/routes/Dashboard.routes.js
@@ -1,9 +1,8 @@
-import express from "express"
-import {getDashboardData} from "../controller/dashboard.controller.js"
-import { protect } from "../middleware/Auth.middleware.js"
+import express from "express";
+import { getDashboardData } from "../controller/dashboard.controller.js";
+import { protect } from "../middleware/Auth.middleware.js";
 
-
-const Router = express.Router()
-Router.get("/",protect, getDashboardData)
+const Router = express.Router();
+Router.get("/", protect, getDashboardData);
 
 export default Router;
