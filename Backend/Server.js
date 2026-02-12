5ae5c22957beb695631ac9c4c597ab2bcf70d720
Kamran Shakib <kamranshakib371@gmail.com>
2026-02-12 20:40:21 +0430

    refactoring project

--- Backend/Server.js
+++ Backend/Server.js
@@ -7,7 +7,7 @@ import incomeRoutes from "./routes/Incode.routes.js"
 import expenseRoutes from "./routes/Expense.routes.js"
 import dashboardRoutes from "./routes/Dashboard.routes.js"
 import path from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url"; 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
