f0d4f1ae4759c9bf81639eb3e659401e9ba2a453
Kamran Shakib <kamranshakib371@gmail.com>
2026-02-27 20:10:01 +0430

    code refactoring and deploy on render

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { SIDE_MENU_DATA } from "../../Utils/data";
-import { UserContext } from "../../context/UserContext";
+import { UserContext } from "../../context/userContext";
 import { useNavigate } from "react-router-dom";
 import CharAvatar from "../Cards/CharAvatar";
 import { LuLogOut } from "react-icons/lu";
