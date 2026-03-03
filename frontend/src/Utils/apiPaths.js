df1d92532d424f0cd622f4fb73f7a7112c07a415
Kamran Shakib <kamranshakib371@gmail.com>
2026-03-03 23:12:45 +0430

    code refactoring

--- frontend/src/Utils/apiPaths.js
+++ frontend/src/Utils/apiPaths.js
@@ -1,5 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
-
+export const BASE_URL = import.meta.env.VITE_API_URL ?? window.location.origin 
 export const API_PATHS = {
   AUTH: {
     LOGIN: "/auth/login",
